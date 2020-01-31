1eaa2d704c5c129344df58d34be97996751a6b14
satyakiwinsome <satyaki.majumder@winsomeitsolutions.com>
2020-01-31 10:23:17 +0530

    first commit

--- newpage/src/App.js
+++ newpage/src/App.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 import './App.css';
 
-import SearchDoctor from "./pages/searchdoctor/searchDoctor";
 
 function App() {
   return (
     <div className="App">
-      <SearchDoctor />
+      
     </div>
   );
 }
