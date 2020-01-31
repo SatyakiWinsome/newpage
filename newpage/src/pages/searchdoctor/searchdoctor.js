fcccef8303eb3a30ab94055b4e5d447c7e3dccc4
satyakiwinsome <satyaki.majumder@winsomeitsolutions.com>
2020-01-31 10:25:53 +0530

    first commit

---
+++ newpage/src/pages/searchdoctor/searchdoctor.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./searchdoctor.css";
+
+class SearchDoctor extends React.Component{
+
+    render(){
+        return(
+            <div className="mainContainer">
+                 <div>
+                     {/* Left Part */}
+                 </div>
+                 <div>
+
+                 </div>
+            </div>
+        )
+    }
+}
+
+export default SearchDoctor;
