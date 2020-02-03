b2c479f9fd2d57e982fa7add12a4d85a2c6c4b6a
satyakiwinsome <satyaki.majumder@winsomeitsolutions.com>
2020-02-03 14:32:04 +0530

    changes made

--- newpage/src/pages/searchdoctor/searchdoctor.js
+++ newpage/src/pages/searchdoctor/searchdoctor.js
@@ -5,6 +5,7 @@ import {Dropdown} from "react-bootstrap";
 import AdvancedSearch from "../../assets/images/advance_search_icon.png";
 import femaleDoc from "../../assets/images/exp_panel11.jpg";
 import tickmark from "../../assets/images/verify_tick_icon.png";
+import docPic121 from "../../assets/images/doctor_art_img.jpg";
 
 import "./searchdoctor.css";
 
@@ -12,10 +13,131 @@ class SearchDoctor extends React.Component{
 
     render(){
         return(
-            <div className="mainContainer">
-                 <div>
+            <div className="container-fluid">
+
                      {/* Left Part */}
-                 </div>
+        <div class="row rowR1">
+          <div class="col-sm-12 col-md-4 col-lg-3 custCol1">
+            <div class="container cont1">
+              <h1>
+                {" "}
+                Search <span style={{ fontSize: "1em" }}>Doctors</span>
+              </h1>
+            </div>
+            <div class="container cont2">
+              <h3>Filter by</h3>
+            </div>
+
+            <div class="container cont3">
+              <h3 style={{ color: "red" }}>Specialities:</h3>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 1
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 2
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 1
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 2
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 1
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 2
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 1
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 2
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 1
+                </label>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" value="" class="inp1" />
+                  Option 2
+                </label>
+              </div>
+            </div>
+
+            <div class="container cont4">
+              <h4 style={{ color: "white" }}>Consultation Fee</h4>
+              <div
+                style={{
+                  display: "flex",
+                  marginTop: "28px",
+                  justifyContent: "center"
+                }}
+              >
+                <h6 style={{ color: "white" }}>0 INR</h6>
+                <br></br>
+                <div class="slidecontainer">
+                  <input
+                    type="range"
+                    min="0"
+                    max="2000"
+                    class="slider"
+                    id="myRange"
+                  />
+                </div>
+                <br></br>
+                <h6 style={{ color: "white" }}>2000 INR</h6>
+              </div>
+            </div>
+            <div class="container cont5">
+              <h1 style={{ color: "red" }}>Gender:</h1>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <label>
+                  <input type="radio" name="option" class="card-input-radio" />
+                  <div class="card1">Male Doctor</div>
+                </label>
+
+                <label>
+                  <input type="radio" name="option" class="card-input-radio" />
+                  <div class="card2">Female Doctor</div>
+                </label>
+              </div>
+              <button type="reset" class="reset">
+                <h3 style={{ borderBottom: "1px solid" }}>Reset filter</h3>
+              </button>
+              <img src={docPic121} alt="doc" class="docpic121"></img>
+            </div>
+          </div>
+
+
+                 
                  <div class="col-sm-12 col-md-8 col-lg-9 customCol1SearchPage">
                     <div className="row1">
                         <div class="row row101">
@@ -62,7 +184,7 @@ class SearchDoctor extends React.Component{
                             </div>
                         </div>
                     </div>
-                    <div class="row">
+                    <div class="row row102">
                         <div className="row2">
                             <span className="row2C1">Featured</span>
                             <span className="row2C2">Doctors</span>
@@ -272,6 +394,7 @@ class SearchDoctor extends React.Component{
                         Load More...
                     </div>
                  </div>
+            </div>
             </div>
         )
     }
